78e67df4732c2a649998240e0de0cd413a46f2b9
Nick Phura <Nick.Phura@quartech.com>
2019-10-17 15:16:13 -0700

    Update versions

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,4 +4,9 @@
 //   }
 // }
 
-// TODO: Enable husky - currently disabled due to a file-path issue when using Angular CLI 7+ - https://github.com/okonet/lint-staged/issues/547
+
+/*
+ * TODO:
+ *  - Enable husky - currently disabled due to a file-path issue when using Angular CLI 7+ - https://github.com/okonet/lint-staged/issues/547
+ *  - Rename this file to .huskyrc for consistency with other similar configs.  Only using .huskyrc.js because it allows comments.
+ */
